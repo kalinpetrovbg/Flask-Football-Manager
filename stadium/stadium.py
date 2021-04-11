82df9459a189ea65ea105a18c3beed3e7bb0cfe8
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-04-11 23:03:51 +0300

    adding files

---
+++ stadium/stadium.py
@@ -0,0 +1,19 @@
+STADIUM_NAMES = {
+    "Camp Nou": 99354,
+    "Wembley Stadium": 90000,
+    "Croke Park": 82300,
+    "Twickenham Stadium": 82000,
+    "Signal Iduna Park": 81359,
+}
+
+
+class Stadium:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+
+    def generate_stadium(self):
+        pass
+
+    def generate_visitors(self):
+        pass
