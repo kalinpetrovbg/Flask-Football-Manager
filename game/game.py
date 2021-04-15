46b3ca125acfb4dff3d87f97dddd35ff18265071
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-04-15 23:21:57 +0300

    update

--- game/game.py
+++ game/game.py
@@ -1,4 +1,6 @@
 import random
+import time
+
 from awayteam import away_team
 from stadium.stadium import Stadium
 from weather.weather_data import WEATHER_TYPES
@@ -8,12 +10,24 @@ away_team_name = away_team
 
 print(f"\n{home_team_name} : {away_team_name} = 0:0")
 
+time.sleep(2)
+
 stadium = Stadium()
 weather = random.choice(WEATHER_TYPES)
 stadium.generate_stadium()
 
 print(stadium.generate_number_spectators(weather))
 
+match_time = 0
+print("Ref blows the whistle and we're under way!")
+
+attack_event = random.choice(list_of_attacks)
+
+def attack(event, team):
+
+
+
+print(attack(attack_event, attacking_team))
 
 
 
