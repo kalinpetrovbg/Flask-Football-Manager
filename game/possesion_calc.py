61fb6838d248c67590db9778a1e97e68ae282a40
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-04-20 22:54:50 +0300

    update

--- game/possesion_calc.py
+++ game/possesion_calc.py
@@ -1,5 +1,12 @@
 from awayteam import *
 from hometeam import *
 
-possession_home_team = 100 * (HOME_TEAMS.midfield / (HOME_TEAMS.midfield + AWAY_TEAMS.midfield))
-possession_away_team = 100 * (AWAY_TEAMS.midfield / (AWAY_TEAMS.midfield + HOME_TEAMS.midfield))
+def calculate_home_team_possession(home_team_mid, away_team_mid):
+    return 100 * (home_team_mid / (home_team_mid + away_team_mid))
+
+def calculate_away_team_possession(home_team_mid, away_team_mid):
+    return 100 * (away_team_mid / (away_team_mid + home_team_mid))
+
+
+possession_home_team = calculate_home_team_possession(HOME_TEAMS.midfield, AWAY_TEAMS.midfield)
+possession_away_team = calculate_away_team_possession(HOME_TEAMS.midfield, AWAY_TEAMS.midfield)
