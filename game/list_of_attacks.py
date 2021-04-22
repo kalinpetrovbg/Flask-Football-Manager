647b12a98477da1775bbfef0acb1bd665a577663
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-04-22 23:14:39 +0300

    update

---
+++ game/list_of_attacks.py
@@ -0,0 +1,36 @@
+from hometeam import *
+from awayteam import *
+
+winner = HOME_TEAMS
+loser = AWAY_TEAM
+scorer = "A"
+defender = "B"
+minutes = 0
+score = 0
+
+LIST_OF_ATTACKS = [
+    f"The bleachers were silenced after {minutes} minutes when {winner}'s {scorer} put the guests "
+    f"ahead {score}, following an attack on the right.",
+
+    f"After {minutes} minutes a combination in the middle resulted in a through ball for {scorer} who "
+    f"increased {winner}'s lead to {score}.",
+
+    f"The home side put themselves ahead {score} after {minutes} minutes of hard work with a breakthrough from "
+    f"{scorer}, who, all by his lonesome, came out of the right wing and hooked the ball over the visitors' keeper.",
+
+    f"{winner} solidified their lead at the {minutes} minute mark, as {scorer} snaked the ball "
+    f"through the central line of defense and finished for {score}.",
+
+    f"Cheers filled the stadium as {scorer} broke through the visitors' central defense "
+    f"to put away the {score} goal for {winner}."
+
+
+
+
+
+
+
+
+
+
+]
