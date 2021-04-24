7cb4e4645bf5f30655500ce26c6ddf6ab8bfca0c
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-04-24 23:11:09 +0300

    fix possesion calc

--- game/possesion_calc.py
+++ game/possesion_calc.py
@@ -8,9 +8,8 @@ def calculate_away_team_possession(home_team_mid, away_team_mid):
     return 100 * (away_team_mid / (away_team_mid + home_team_mid))
 
 def print_possession_message(home_team_name, away_team_name):
-    return f"Ball Possession was: {home_team_name} {int(possession_home_team)}% / {away_team_name} " \
-           f"{int(possession_away_team)}%"
-
+    return f"Ball Possession was: {home_team_name} {round(possession_home_team)}% / {away_team_name} " \
+           f"{round(possession_away_team)}%"
 
 
 possession_home_team = calculate_home_team_possession(HOME_TEAMS.midfield, AWAY_TEAM.midfield)
