e90430512c2039953aba0272ea2843f9ac3449ec
Kalin <kalinpetrovbg@gmail.com>
2022-05-13 23:02:50 +0300

    adding fixtures

--- tests/fixures/base_fixture.py
+++ tests/fixures/base_fixture.py
@@ -6,12 +6,26 @@ from app import app
 
 
 @pytest.fixture
-def client():
-    """Configures the app for testing
-    Sets app config variable ``TESTING`` to ``True``
-    :return: App for testing
-    """
-    # app.config['TESTING'] = True
-    client = app.test_client()
+def test_app():
+    test_app = app
+    test_app.config.update({
+        "TESTING": True,
+    })
 
-    yield client
+    yield app
+
+
+@pytest.fixture()
+def client(test_app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(test_app):
+    return app.test_cli_runner()
+
+
+
+def test_request_example(client):
+    response = client.get("/")
+    assert response.status_code == 200
